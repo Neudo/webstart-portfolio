cfb67314c82b1e3a4481fa992aa7bd7dfbd66aef
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-03-30 16:37:41 +0200

    fix netifly

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -1,1 +1,17 @@
-import {IWork} from "@/@types/work";
+import {FC} from "react";
+import Link from "next/link";
+
+
+
+
+const TestComponent: FC = () =>
+    (
+        <div className="container">
+            <div className="wrapper-text w-1/2 m-auto">
+                <h2 className="mb-7">Test</h2>
+            </div>
+        </div>
+
+    )
+
+export default TestComponent
